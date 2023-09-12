fc0167c5cd79c9afc41e31eb2441e0e82be20de2
Aron Gulyas <aron.gulyas.hu@gmail.com>
2023-09-12 11:44:43 +0200

    configloader update

--- config/src/main/java/aron/library/config/loader/ConfigLoader.java
+++ config/src/main/java/aron/library/config/loader/ConfigLoader.java
@@ -1,8 +1,6 @@
 package aron.library.config.loader;
 
 import aron.library.config.encryptor.ConfigEncryptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
@@ -12,16 +10,17 @@ import java.util.Properties;
  * Opens and loads the encrypted configuration file and makes the properties available to different classes.
  */
 public class ConfigLoader {
-    private static final Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
     private String keyFile, confFile;
     private Properties properties;
 
     /**
      * Loads the configuration when the application is started.
-     * @param keyFile Path of the decryption key file.
+     *
+     * @param keyFile  Path of the decryption key file.
      * @param confFile Path of the encrypted configuration file.
      */
-    public ConfigLoader(final String keyFile, final String confFile) throws IOException {
+    public ConfigLoader( final String keyFile, final String confFile )
+    throws IOException {
         this.keyFile = keyFile;
         this.confFile = confFile;
         loadAndDecryptConfigFile();
@@ -32,9 +31,11 @@ public class ConfigLoader {
      * db_user_properties -- properties of "user" db, which only handles user related data
      * db_work_properties -- properties of "work" db, which only handles application related data
      * app_properties     -- properties of cornerstone itself (e.g.: rotation interval, node name etc.)
+     *
      * @throws IOException When the underlying ConfigEncryptDecrypt files to load the keyfile or fails to decrypt the config file.
      */
-    public void loadAndDecryptConfigFile() throws IOException {
+    public void loadAndDecryptConfigFile()
+    throws IOException {
         final SecretKey secretKey = ConfigEncryptor.loadAESKeyFromFile(keyFile);
         properties = ConfigEncryptor.decryptConfig(secretKey, confFile);
     }
@@ -42,14 +43,14 @@ public class ConfigLoader {
     /**
      * @param keyFile Path of the key file to be used for decrypting the configuration file.
      */
-    public void setKeyFile(String keyFile) {
+    public void setKeyFile( String keyFile ) {
         this.keyFile = keyFile;
     }
 
     /**
      * @param confFile Path of the configuration file.
      */
-    public void setConfFile(String confFile) {
+    public void setConfFile( String confFile ) {
         this.confFile = confFile;
     }
 
